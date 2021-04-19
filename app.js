54e5454e07384923e63cd3fe7674766d907f8015
lalitaly <lalitaly@gmail.com>
2021-04-19 21:53:28 +0700

    make it better2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = process.env.PORT || 5000
 const app = express()
 
-const data = { name: "egci427"}
+const data = { name: "egco427"}
 app.get('/', (req, res) => {
   res.json(data)
 })
