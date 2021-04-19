fd342497c3ff8f898b66b213c248a97d831f472c
lalitaly <lalitaly@gmail.com>
2021-04-19 22:56:25 +0700

    deploy auto2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = process.env.PORT || 5000
 const app = express()
 
-const data = { name: "egco427-heroku-autorepo"}
+const data = { name: "egco427-heroku-autorepo-test-review"}
 app.get('/', (req, res) => {
   res.json(data)
 })
