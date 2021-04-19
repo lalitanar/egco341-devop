e527cc15270a868017f96b337e9e2c28ac4425b8
lalitaly <lalitaly@gmail.com>
2021-04-19 22:04:10 +0700

    deploy git

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = process.env.PORT || 5000
 const app = express()
 
-const data = { name: "egco427"}
+const data = { name: "egco427-gitrepo"}
 app.get('/', (req, res) => {
   res.json(data)
 })
